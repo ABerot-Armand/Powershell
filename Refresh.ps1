68be771960c520e5d04d0c0ce8f73cd923ac6ba7
ABerot-Armand <148052820+ABerot-Armand@users.noreply.github.com>
2024-02-15 13:34:50 +0100

    Add files via upload
    Ce script a été créer lors du CTF Secu'RT pour actualiser l'affichage du classement (qui était hébergé de façon statique sur le serveur)

---
+++ Refresh.ps1
@@ -0,0 +1,10 @@
+﻿$medge = Start-Process -PassThru -FilePath microsoft-edge:http://192.168.33.250/scoreboard
+$wshell = New-Object -ComObject wscript.shell
+
+Start-Sleep -Seconds 15                  ### increase time for slow connection
+while ( $true ) ### activate the desired window
+{
+    $wshell.AppActivate($medge.Id)
+    $wshell.SendKeys('{f5}')             ### send keys only if successfully activated
+    Start-Sleep -Seconds 15
+}
